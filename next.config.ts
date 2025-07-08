896a5f48e35050496ef6f64bf128de84e8fcfe16
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-07-08 20:09:29 -0300

    first setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["chimp.lon1.cdn.digitaloceanspaces.com"],
+  },
 };
 
 export default nextConfig;
